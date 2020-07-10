78f3cdaecb2c84c39573e5a52e9be5e7659398c3
qiandongdong <qiandongdong@basestonedata.com>
2020-07-10 16:38:20 +0800

    线程池

--- src/main/Java/Thread/MyExecutorPoolTest.java
+++ src/main/Java/Thread/MyExecutorPoolTest.java
@@ -13,11 +13,11 @@ public class MyExecutorPoolTest {
     public static void main(String[] args) {
         ThreadPoolExecutor executor = new ThreadPoolExecutor(2,
                 3, 60,
-                TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(10),
+                TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(1),
                 new MyThreadFactory("TestPool"),
                 new MyRejectedExecutionHandler());
         //分成若干组个任务
-        List<List<Integer>> lists = ListUtils.averageDistribution(getList(1000), 12);
+        List<List<Integer>> lists = ListUtils.averageDistribution(getList(15), 5);//当任务个数大于核心线程数量，有任务可能会被丢弃【线程池中线程耗尽，阻塞队列满】
         for (int i = 0; i < lists.size(); i++) {
             executor.execute(new MyTask("Task"+i,lists.get(i)));
         }
--- src/main/Java/Thread/MyTask.java
+++ src/main/Java/Thread/MyTask.java
@@ -8,7 +8,7 @@ public class MyTask implements Runnable {
 
     private String name;
 
-    public MyTask(String name,List<Integer> list) {
+    public MyTask(String name, List<Integer> list) {
         this.name = name;
         this.list = list;
     }
@@ -20,6 +20,6 @@ public class MyTask implements Runnable {
             System.out.println(list.get(i));
         }
         long end = System.currentTimeMillis();
-        System.out.println(Thread.currentThread().getName() + "***************执行完毕!执行时间:"+(end - start)+"ms");
+        System.out.println(Thread.currentThread().getName() + "***************执行完毕!执行时间:" + (end - start) + "ms");
     }
 }
